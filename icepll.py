e2c116a77cd858e0f344b3cb74b090123caa42ae
Olof Kindgren <olof.kindgren@gmail.com>
2018-12-03 12:18:52 +0100

    Add module parameter to icepll

--- icepll.py
+++ icepll.py
@@ -4,15 +4,19 @@ import subprocess
 
 class IcepllGenerator(Generator):
     def run(self):
-        fin      = self.config.get('freq_in') or 12
-        fout     = self.config.get('freq_out') or 60
-        filename = self.config.get('filename') or 'pll.v'
+        fin      = self.config.get('freq_in', 12)
+        fout     = self.config.get('freq_out', 60)
+        module   = self.config.get('module', False)
+        filename = self.config.get('filename', 'pll.v' if module else 'pll.vh')
 
-        args = ['icepll', '-m', '-f', filename, '-i', str(fin), '-o', str(fout)]
+        args = ['icepll', '-f', filename, '-i', str(fin), '-o', str(fout)]
+        if module:
+            args.append('-m')
         rc = subprocess.call(args)
         if rc:
             exit(1)
-        self.add_files([{filename : {'file_type' : 'verilogSource'}}])
+        self.add_files([{filename : {'file_type' : 'verilogSource',
+                                     'is_include_file' : not module}}])
 
 g = IcepllGenerator()
 g.run()
